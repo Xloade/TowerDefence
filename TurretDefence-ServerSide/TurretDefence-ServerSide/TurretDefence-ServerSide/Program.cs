68c54a3b02d23921e9be349d80e32bb9b3c12d8a
x-loade <dainius1999@hotmail.lt>
2021-09-19 14:07:08 +0300

    server side start

---
+++ TurretDefence-ServerSide/TurretDefence-ServerSide/TurretDefence-ServerSide/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TurretDefence_ServerSide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
