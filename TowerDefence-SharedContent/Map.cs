0bcce627fe7e6f01613278cf85916d43e7bb7cdc
x-loade <dainius1999@hotmail.lt>
2021-10-05 21:06:40 +0300

    map to json

--- TowerDefence-SharedContent/Map.cs
+++ TowerDefence-SharedContent/Map.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,13 +7,24 @@ namespace TowerDefence_SharedContent
 {
     public class Map
     {
-        Player player1;
-        Player player2;
+        public Player player1;
+        public Player player2;
 
         public Map()
         {
             player1 = new Player();
             player2 = new Player();
+        }
+
+        public Player GetPlayer(PlayerType type)
+        {
+            return type == PlayerType.PLAYER1 ? player1 : player2;
+        }
+
+        public string ToJson()
+        {
+            JObject mapJson = (JObject)JToken.FromObject(this);
+            return mapJson.ToString();
         }
     }
 }
