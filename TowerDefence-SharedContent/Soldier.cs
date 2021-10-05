502635dad52c461b8660520813747cfd29bfbd84
x-loade <dainius1999@hotmail.lt>
2021-10-05 21:07:40 +0300

    added soldier constructor

--- TowerDefence-SharedContent/Soldier.cs
+++ TowerDefence-SharedContent/Soldier.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace TowerDefence_SharedContent
 {
@@ -11,5 +12,12 @@ namespace TowerDefence_SharedContent
         public int[] BuyPrice { get; set; }
         public double Speed { get; set; }
         public double[] Hitpoints { get; set; }
+
+        public Point Coordinates { get; set; }
+
+        public Soldier()
+        {
+            Coordinates = new Point(0, 450);
+        }
     }
 }
