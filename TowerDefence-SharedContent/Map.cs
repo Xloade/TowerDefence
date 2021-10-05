dc0ec781a63318e68dce06bf9a049cf7cf96a215
x-loade <dainius1999@hotmail.lt>
2021-10-05 18:28:06 +0300

    added map

---
+++ TowerDefence-SharedContent/Map.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowerDefence_SharedContent
+{
+    class Map
+    {
+        Player player1;
+        Player player2;
+        List<Soldier> soldiers;
+        List<Tower> towers;
+
+        public Map()
+        {
+            player1 = new Player();
+            player2 = new Player();
+            soldiers = new List<Soldier>();
+            towers = new List<Tower>();
+        }
+    }
+}
