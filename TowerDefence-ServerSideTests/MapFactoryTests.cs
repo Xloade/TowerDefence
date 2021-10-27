95c9abb758559827ee9737e4f9d989126d158582
x-loade <dainius1999@hotmail.lt>
2021-10-27 11:05:31 +0300

    new mapFactory tests

---
+++ TowerDefence-ServerSideTests/MapFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TowerDefence_ServerSide;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using TowerDefence_SharedContent;
+
+namespace TowerDefence_ServerSide.Tests
+{
+    [TestClass()]
+    public class MapFactoryTests
+    {
+        private MapFactory mapFactory;
+        [TestInitialize()]
+        public void Setup()
+        {
+            mapFactory = new MapFactory();
+        }
+        [DataTestMethod()]
+        [DataRow("Summer", "Green")]
+        [DataRow("Spring", "GreenYellow")]
+        [DataRow("Winter", "LightCyan")]
+        [DataRow("Autumn", "OrangeRed")]
+        [DataRow("RandromName", "OrangeRed")]
+        public void CreateMap_checksIfcreatedCorrect_allMatches(string mapType, String correctColor)
+        {
+            Color color = Color.FromName(correctColor);
+            Map map = mapFactory.CreateMap(mapType);
+            Assert.AreEqual(map.mapColor,color);
+        }
+        [DataTestMethod()]
+        [DataRow("Summer", "Blue")]
+        [DataRow("RandromName", "RandomColor")]
+        public void CreateMap_checksIfcreatedCorrect_allDoesntMatch(string mapType, String correctColor)
+        {
+            Color color = Color.FromName(correctColor);
+            Map map = mapFactory.CreateMap(mapType);
+            Assert.AreNotEqual(map.mapColor, color);
+        }
+    }
+}
