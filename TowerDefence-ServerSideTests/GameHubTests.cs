3dc15ffa4365ba6d134e1679cdef6d023d173859
x-loade <dainius1999@hotmail.lt>
2021-10-27 13:24:19 +0300

    added tests or GameHub

---
+++ TowerDefence-ServerSideTests/GameHubTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TowerDefence_ServerSide;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using TowerDefence_SharedContent;
+
+
+namespace TowerDefence_ServerSide.Tests
+{
+    [TestClass()]
+    public class GameHubTests
+    {
+
+        private GameHub gameHub;
+        private MapController mapController;
+        [TestInitialize()]
+        public void Setup()
+        {
+            gameHub = new GameHub();
+            var gameHubMock = new Mock<Microsoft.AspNetCore.SignalR.IHubContext<GameHub>>();
+            MapController.setIHubContext(gameHubMock.Object);
+            MapController.createInstance("Winter");
+            mapController = MapController.getInstance();
+        }
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            MapController.removeInstance();
+        }
+        [TestMethod()]
+        public void createMapTest()
+        {
+            MapController.removeInstance();
+            gameHub.createMap("Winter");
+            Assert.IsNotNull(MapController.getInstance());
+        }
+
+        [TestMethod()]
+        public void buySoldierTest()
+        {
+            gameHub.buySoldier(PlayerType.PLAYER1);
+            Assert.AreEqual(1, mapController.map.GetPlayer(PlayerType.PLAYER1).soldiers.Count);
+        }
+
+        [TestMethod()]
+        public void buyTowerTest()
+        {
+            gameHub.buyTower(PlayerType.PLAYER1);
+            Assert.AreEqual(1, mapController.map.GetPlayer(PlayerType.PLAYER1).towers.Count);
+        }
+
+        [TestMethod()]
+        public void restartGameTest()
+        {
+            gameHub.restartGame();
+            Assert.AreNotSame(mapController, MapController.getInstance());
+        }
+
+        [TestMethod()]
+        public void deleteTowerTest()
+        {
+            gameHub.buyTower(PlayerType.PLAYER1);
+            gameHub.deleteTower(PlayerType.PLAYER1);
+            Assert.AreEqual(0, mapController.map.GetPlayer(PlayerType.PLAYER1).towers.Count);
+        }
+    }
+}
