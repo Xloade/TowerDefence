ec2f061b29629439874efcd729889813b4291f5f
x-loade <dainius1999@hotmail.lt>
2021-11-06 21:55:15 +0200

    lazy image

---
+++ TowerDefence-ClientSide/LazyImageDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace TowerDefence_ClientSide
+{
+    class LazyImageDictionary
+    {
+        private Dictionary<string, Image> images;
+
+        public LazyImageDictionary()
+        {
+            this.images = new Dictionary<string, Image>();
+        }
+
+        public Image get(string path)
+        {
+            if (!images.ContainsKey(path))
+            {
+                images.Add(path, Image.FromFile(path));
+            }
+            return images[path];
+        }
+    }
+}
