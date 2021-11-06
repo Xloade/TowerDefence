d839a1626c265ae1f0c63e135eb5694de82e1aca
x-loade <dainius1999@hotmail.lt>
2021-11-06 21:18:49 +0200

    new sprite paths

--- TowerDefence-SharedContent/SpritePaths.cs
+++ TowerDefence-SharedContent/SpritePaths.cs
@@ -7,16 +7,17 @@ namespace TowerDefence_SharedContent
 {
     public static class SpritePaths
     {
+        readonly public static string dir = "../../../Sprites";
         public static string getTower(PlayerType playerType, TowerType towerType)
         {
             switch(towerType)
             {
                 case TowerType.Minigun:
-                    return playerType == PlayerType.PLAYER1 ? "../../../Sprites/bulletTower(Blue).png" : "../../../Sprites/bulletTower(Red).png";
+                    return playerType == PlayerType.PLAYER1 ? $"{dir}/bulletTower(Blue).png" : $"{dir}/bulletTower(Red).png";
                 case TowerType.Rocket:
-                    return playerType == PlayerType.PLAYER1 ? "../../../Sprites/rocketTower(Blue).png" : "../../../Sprites/rocketTower(Red).png";
+                    return playerType == PlayerType.PLAYER1 ? $"{dir}/rocketTower(Blue).png" : $"{dir}/rocketTower(Red).png";
                 case TowerType.Laser:
-                    return playerType == PlayerType.PLAYER1 ? "../../../Sprites/laserTower(Blue).png" : "../../../Sprites/laserTower(Red).png";
+                    return playerType == PlayerType.PLAYER1 ? $"{dir}/laserTower(Blue).png" : $"{dir}/laserTower(Red).png";
                 default:
                     return "";
             }
@@ -24,12 +25,17 @@ namespace TowerDefence_SharedContent
 
         public static string getSoldier(PlayerType type)
         {
-            return type == PlayerType.PLAYER1 ? @"../../../Sprites/soldier(Blue).png" : @"../../../Sprites/soldier(Red).png";
+            return type == PlayerType.PLAYER1 ? $"{dir}/soldier(Blue).png" : $"{dir}/soldier(Red).png";
         }
 
         public static string getBullet()
         {
-            return @"../../../Sprites/bullet.png";
+            return $"{dir}/bullet.png";
+        }
+
+        public static string getMap(String mapType)
+        {
+            return $"{dir}/{mapType}Map.png";
         }
     }
 }
