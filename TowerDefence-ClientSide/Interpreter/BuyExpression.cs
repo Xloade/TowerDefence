bba40767a8c15a2aa664fab5d682df4b810111b7
ziltom <ziltom@DESKTOP-8T5UFJD>
2021-12-12 01:06:26 +0200

    inrpteret

---
+++ TowerDefence-ClientSide/Interpreter/BuyExpression.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefence_ClientSide.Interpreter
+{
+    public class BuyExpression : TerminalExpression
+    {
+        public override string Qualify(string input)
+        {
+            return input.StartsWith("buy") ? "buy" : "";
+        }
+    }
+}
---
+++ TowerDefence-ClientSide/Interpreter/ElementTypeExpression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TowerDefence_ClientSide.Interpreter
+{
+    public class ElementTypeExpression : NonTerminalExpression
+    {
+        public override List<string> ElementTypes()
+        {
+            return new List<string>
+            {
+                "hitpoints",
+                "speed",
+                "minigun",
+                "laser",
+                "rocket",
+            };
+        }
+    }
+}
